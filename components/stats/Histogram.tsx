d1e056f1c5ee1a5d0acbfb1f69659f1ca8a2b358
Lucio Villa <lucio310@gmail.com>
2022-01-27 09:36:05 -0500

    update histogram

--- components/stats/Histogram.tsx
+++ components/stats/Histogram.tsx
@@ -6,12 +6,13 @@ type Props = {
 }
 
 export const Histogram = ({ gameStats }: Props) => {
-  const { totalGames, winDistribution } = gameStats
+  const { winDistribution } = gameStats
+  const maxValue = Math.max(...winDistribution)
 
   return (
     <div className="columns-1 justify-left m-2 text-sm">
       {winDistribution.map((value, i) => (
-        <Progress key={i} index={i} size={95 * (value / totalGames)} label={String(value)} />
+        <Progress key={i} index={i} size={90 * (value / maxValue)} label={String(value)} />
       ))}
     </div>
   )

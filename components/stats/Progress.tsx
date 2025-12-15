9f0dc19c60eb63cd089e7ff9362dbbac2736e701
Lucio Villa <lucio310@gmail.com>
2025-12-14 21:32:40 -0500

    feat: Integrate a new UI component system with theming, toast notifications, and utility functions.

--- components/stats/Progress.tsx
+++ components/stats/Progress.tsx
@@ -7,11 +7,11 @@ type Props = {
 export const Progress = ({ index, size, label }: Props) => {
   return (
     <div className="flex justify-left m-1">
-      <div className="items-center justify-center w-2">{index + 1}</div>
+      <div className="items-center justify-center w-2 text-slate-300">{index + 1}</div>
       <div className="w-full ml-2">
         <div
           style={{ width: `${6 + size}%` }}
-          className="bg-green-500 text-xs font-medium text-white text-center p-0.5"
+          className="bg-green-500 text-xs font-medium text-slate-200 text-center p-0.5"
         >
           {label}
         </div>
